e90b23b9ebfc4ade8e4b96e438824386ff14a25b
ViktoriaKukh <bstet@mail.ru>
2022-12-09 01:07:23 +0500

    second task. player vs bot

--- second.py
+++ second.py
@@ -6,22 +6,55 @@
 # a) Добавьте игру против бота
 # b) Подумайте как наделить бота ""интеллектом""
 
-def now_turn(swt: int, player: int):
-    player_move = int(input(f'{player} игрок, сейчас Ваш ход. на столе {swt} конфет. Сколько конфет вы возьмете? '))
+# player vs player
+# # def now_turn(swt: int, player: int):
+# #     player_move = int(input(f'{player} игрок, сейчас Ваш ход. на столе {swt} конфет. Сколько конфет вы возьмете? '))
+# #     while (player_move <= 0) or (player_move > 28):
+# #         player_move = int(input("Количество конфет за 1 раз должно быть от 1 до 28: "))
+# #     swt -= player_move
+# #     if swt > 0:
+# #         if player == 2:
+# #             now_turn(swt, 1)
+# #         else:
+# #             now_turn(swt, 2)
+# #     else:
+# #         print(f'Игрок №{player}, вы победили!!!')
+    
+# from random import randint
+# sweets = 2021
+# now_playing = randint(1,2)
+# print('Начнем игру!')
+# print(f'Игру начинает игрок номер {now_playing}')
+# now_turn(sweets, now_playing)
+
+
+def now_turn(swt: int):
+    player_move = int(input(f'Cейчас Ваш ход. на столе {swt} конфет. Сколько конфет вы возьмете? '))
     while (player_move <= 0) or (player_move > 28):
         player_move = int(input("Количество конфет за 1 раз должно быть от 1 до 28: "))
     swt -= player_move
     if swt > 0:
-        if player == 2:
-            now_turn(swt, 1)
-        else:
-            now_turn(swt, 2)
+        bot_turn(swt)
     else:
-        print(f'Игрок №{player}, вы победили!!!')
-    
+        print(f'Dы победили!!!')
+
+def bot_turn(sweets_: int):
+    player_move = randint(1,28)
+    print(f'Cейчас ходит бот. На столе {sweets_} конфет. Бот берёт {player_move} конфет.')
+    sweets_ -= player_move
+    if sweets_ > 0:
+        now_turn(sweets_)
+    else:
+        print(f'Победил бот!!!')
+
+
 from random import randint
 sweets = 2021
 now_playing = randint(1,2)
 print('Начнем игру!')
-print(f'Игру начинает игрок номер {now_playing}')
-now_turn(sweets, now_playing)
+if now_playing == 1:
+    print(f'Игру начинает бот.')
+    bot_turn(sweets)
+else:
+    print(f'Игру начинает игрок!')
+    now_turn(sweets)
